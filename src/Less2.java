951e58cc1f0db289ebd08ef450b5a33203241aa4
ShiryevIgorVl <ishirayev@gmail.com>
2020-07-21 15:36:01 +0300

    1 metod

--- src/Less2.java
+++ src/Less2.java
@@ -4,7 +4,7 @@ public class Less2 {
 
     public static void inverArr () {
         int [] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i <= arr.length ; i++) {
+        for (int i = 0; i <= arr.length; i++) {
             if (arr[i] == 1) {
                 arr [i] = 0;
             } else {
