32cd004eaef046f454029af521b7aed317bba133
ShiryevIgorVl <ishirayev@gmail.com>
2020-07-21 16:45:36 +0300

    1 metod

--- src/Less2.java
+++ src/Less2.java
@@ -3,7 +3,7 @@ public class Less2 {
 
     public static void inverArr () {
         int [] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i <= arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
                 arr [i] = 0;
             } else {
@@ -15,9 +15,19 @@ public class Less2 {
 
     public static void complArr() {
         int [] arr = new int[8];
-        for (int i = 0; i <= arr.length ; i++) {
+        for (int i = 1; i < arr.length ; i++) {
             arr[i+1] = arr[i] + 3;
         }
         System.out.println(arr);
     }
-}
+
+    public static void lesSixmultiplybytwoArr() {
+       int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr [i] = arr [i] * 2;
+            }
+        }
+        System.out.println(arr);
+    }
+    }
