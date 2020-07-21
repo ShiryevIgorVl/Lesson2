6c3c35e62c4f4b3e2ce776b4ca95be735fcc5b89
ShiryevIgorVl <ishirayev@gmail.com>
2020-07-21 15:25:10 +0300

    1 metod

--- src/Less2.java
+++ src/Less2.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 public class Less2 {
 
     public static void inverArr () {
-        
+        int [] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i <= arr.length ; i++) {
+            if (arr[i] == 1) {
+                arr [i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
+
 }
