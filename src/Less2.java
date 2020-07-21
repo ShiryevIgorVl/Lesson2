7bc243268af710c9bd091bc88fbc11d5929bbfc3
ShiryevIgorVl <ishirayev@gmail.com>
2020-07-21 17:15:37 +0300

    1 metod

--- src/Less2.java
+++ src/Less2.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Less2 {
 
-    public static void inverArr () {
+    public static void inverArr (String[] args) {
         int [] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
@@ -14,7 +14,7 @@ public class Less2 {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void complArr() {
+    public static void complArr(String[] args) {
         int [] arr = new int[8];
         for (int i = 1; i < arr.length ; i++) {
             arr[i+1] = arr[i] + 3;
@@ -22,7 +22,7 @@ public class Less2 {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void lesSixmultiplybytwoArr() {
+    public static void lesSixmultiplybytwoArr(String[] args) {
        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
@@ -31,4 +31,14 @@ public class Less2 {
         }
         System.out.println(Arrays.toString(arr));
     }
+    public static void diagonalElementsOfaUnit(String[] args) {
+        int a = 10;
+        int b = 10;
+        int [][] arr = new int[a][b];
+        for (int i = 0; i < arr.length; i++) {
+            if (a == b){
+                arr [a][b] = 1;
+            }
+        }
+        }
     }
