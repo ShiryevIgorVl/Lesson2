772f95d59ad829442c9bb6ce8a00f032a80bc45e
ShiryevIgorVl <ishirayev@gmail.com>
2020-07-23 07:34:23 +0300

    itog

--- src/Less2.java
+++ src/Less2.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Less2 {
 
     public static void inverArr(String[] args) {
@@ -11,15 +9,15 @@ public class Less2 {
                 arr[i] = 1;
             }
         }
-      //  System.out.println(Arrays.toString(arr));
+        //  System.out.println(Arrays.toString(arr));
     }
 
     public static void complArr(String[] args) {
         int[] arr = new int[8];
         for (int i = 1; i < arr.length; i++) {
-            arr[i] = arr[i-1] + 3;
+            arr[i] = arr[i - 1] + 3;
         }
-      //  System.out.println(Arrays.toString(arr));
+        //  System.out.println(Arrays.toString(arr));
     }
 
     public static void lesSixmultiplybytwoArr(String[] args) {
@@ -29,10 +27,10 @@ public class Less2 {
                 arr[i] = arr[i] * 2;
             }
         }
-       // System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(arr));
     }
 
-    public static void main(String[] args) {
+    public static void diagonalelements1(String[] args) {
         int[][] arr = new int[5][5];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) //Второй цикл только для печати
@@ -42,9 +40,34 @@ public class Less2 {
                 System.out.printf("%2d", arr[i][j]);
             }
             System.out.println();
-            }
-
         }
+
+    }
+
+    public static int maxArr(String[] args) {
+        int[] arr = {6, 5, 12, 33, 11, 15, 34, 77};
+        int maxArr = arr[0];
+        int minArr = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (maxArr <= arr[i]) {
+                maxArr = arr[i];
+            }
+        }
+        return maxArr;
+    }
+
+    public static int minArr(String[] args) {
+        int[] arr = {6, 5, 12, 33, 11, 15, 34, 77};
+        int minArr = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (minArr >= arr[i]) {
+                minArr = arr[i];
+            }
+        }
+        return minArr;
+     }
+
 
     }
 
